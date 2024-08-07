3b8aa4e518cc69f5e84fb6dbf56c46e9ac3a7b54
08786218654 <leandro.zuza@connectbr.dev>
2024-08-07 19:48:54 -0300

    Init project

---
+++ src/main/java/api_adocao/ServletInitializer.java
@@ -0,0 +1,13 @@
+package api_adocao;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApiAdocaoApplication.class);
+	}
+
+}
