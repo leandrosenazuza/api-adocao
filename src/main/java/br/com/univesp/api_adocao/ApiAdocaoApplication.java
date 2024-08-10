5283542997eec98fcbe8c4f757d393819e61f02a
08786218654 <leandro.zuza@connectbr.dev>
2024-08-10 10:30:07 -0300

    Init

---
+++ src/main/java/br/com/univesp/api_adocao/ApiAdocaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.univesp.api_adocao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAdocaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAdocaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/univesp/api_adocao/ApiAdocaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.univesp.api_adocao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdocaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
