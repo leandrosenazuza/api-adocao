fc45b9368845361ddb93184cfefd4682c5a6fe95
Leandro Zuza <lendro.zuza@connectbr.dev>
2024-09-08 23:30:18 -0300

    Fix: Banco de dados.

---
+++ src/test/java/api_adocao/api_adocao/ApiAdocaoApplicationTests.java
@@ -0,0 +1,13 @@
+package api_adocao.api_adocao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAdocaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
