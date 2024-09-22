87edf1d08dbf0738574e12c6c7b0180a99217bc2
Leandro Zuza <lendro.zuza@connectbr.dev>
2024-09-22 16:43:08 -0300

    Feat: Swagger implementado

--- database/scripts.sql
+++ database/scripts.sql
@@ -6,11 +6,21 @@ CREATE TABLE especie (
                          descricao_especie VARCHAR(255) NOT NULL
 );
 
+-- Inserções na tabela 'especie'
+INSERT INTO especie (id, descricao_especie) VALUES (1, 'Gato');
+INSERT INTO especie (id, descricao_especie) VALUES (2, 'Cachorro');
+INSERT INTO especie (id, descricao_especie) VALUES (3, 'Pássaro');
+
 -- Criação da tabela 'porte'
 CREATE TABLE porte (
                        id BIGSERIAL PRIMARY KEY,
                        descricao_porte VARCHAR(255) NOT NULL
 );
+
+-- Inserções na tabela 'porte'
+INSERT INTO porte (id, descricao_porte) VALUES (1, 'Pequeno');
+INSERT INTO porte (id, descricao_porte) VALUES (2, 'Médio');
+INSERT INTO porte (id, descricao_porte) VALUES (3, 'Grande');
 
 -- Criação da tabela 'raca' (agora relacionando-se com porte e especie)
 CREATE TABLE raca (
@@ -20,17 +30,36 @@ CREATE TABLE raca (
                       especie_id BIGINT NOT NULL REFERENCES especie(id) ON DELETE RESTRICT
 );
 
+-- Inserções na tabela 'raca'
+-- Labrador é um cão de porte grande
+INSERT INTO raca (descricao_raca, porte_id, especie_id) VALUES ('Labrador', 3, 2);
+-- Poodle é um cão de porte médio
+INSERT INTO raca (descricao_raca, porte_id, especie_id) VALUES ('Poodle', 2, 2);
+-- Siamês é um gato de porte pequeno
+INSERT INTO raca (descricao_raca, porte_id, especie_id) VALUES ('Siamês', 1, 1);
+
+
 -- Criação da tabela 'comportamento'
 CREATE TABLE comportamento (
                                id BIGSERIAL PRIMARY KEY,
                                descricao_comportamento VARCHAR(255) NOT NULL
 );
 
+-- Inserções na tabela 'comportamento'
+INSERT INTO comportamento (id, descricao_comportamento) VALUES (1, 'Calmo');
+INSERT INTO comportamento (id, descricao_comportamento) VALUES (2, 'Agitado');
+INSERT INTO comportamento (id, descricao_comportamento) VALUES (3, 'Brincalhão');
+
 -- Criação da tabela 'cirurgia'
 CREATE TABLE cirurgia (
                           id BIGSERIAL PRIMARY KEY,
                           descricao_cirurgia VARCHAR(255) NOT NULL
 );
+-- Inserções na tabela 'cirurgia'
+INSERT INTO cirurgia (id, descricao_cirurgia) VALUES (1, 'Castração');
+INSERT INTO cirurgia (id, descricao_cirurgia) VALUES (2, 'Retirada de Tumor');
+INSERT INTO cirurgia (id, descricao_cirurgia) VALUES (3, 'Amputação');
+
 
 -- Criação da tabela 'animal'
 CREATE TABLE animal (
@@ -47,34 +76,6 @@ CREATE TABLE animal (
                         descricao_animal TEXT,
                         foto TEXT
 );
-
--- Inserções na tabela 'porte'
-INSERT INTO porte (descricao_porte) VALUES ('Pequeno');
-INSERT INTO porte (descricao_porte) VALUES ('Médio');
-INSERT INTO porte (descricao_porte) VALUES ('Grande');
-
--- Inserções na tabela 'especie'
-INSERT INTO especie (descricao_especie) VALUES ('Gato');
-INSERT INTO especie (descricao_especie) VALUES ('Cachorro');
-INSERT INTO especie (descricao_especie) VALUES ('Pássaro');
-
--- Inserções na tabela 'raca'
--- Labrador é um cão de porte grande
-INSERT INTO raca (descricao_raca, porte_id, especie_id) VALUES ('Labrador', 3, 2);
--- Poodle é um cão de porte médio
-INSERT INTO raca (descricao_raca, porte_id, especie_id) VALUES ('Poodle', 2, 2);
--- Siamês é um gato de porte pequeno
-INSERT INTO raca (descricao_raca, porte_id, especie_id) VALUES ('Siamês', 1, 1);
-
--- Inserções na tabela 'comportamento'
-INSERT INTO comportamento (descricao_comportamento) VALUES ('Calmo');
-INSERT INTO comportamento (descricao_comportamento) VALUES ('Agitado');
-INSERT INTO comportamento (descricao_comportamento) VALUES ('Brincalhão');
-
--- Inserções na tabela 'cirurgia'
-INSERT INTO cirurgia (descricao_cirurgia) VALUES ('Castração');
-INSERT INTO cirurgia (descricao_cirurgia) VALUES ('Retirada de Tumor');
-INSERT INTO cirurgia (descricao_cirurgia) VALUES ('Amputação');
 
 -- Inserindo um Labrador de porte grande e espécie canina
 INSERT INTO animal (nome, idade, raca_id, comportamento_id, cirurgia_id, is_castrado, is_vermifugado, is_vacinado, is_cirurgia, descricao_animal, foto)
