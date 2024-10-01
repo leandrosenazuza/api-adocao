9ad23116938b208051143d52614c7325ca40d6ac
Leandro Zuza <lendro.zuza@connectbr.dev>
2024-10-01 08:54:54 -0300

    Criação do endpoint especie/all

--- src/main/java/api_adocao/Controller/EspecieController.java
+++ src/main/java/api_adocao/Controller/EspecieController.java
@@ -1,9 +1,24 @@
 package api_adocao.Controller;
 
+import api_adocao.Model.Animal;
+import api_adocao.Model.Especie;
+import api_adocao.Service.EspecieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/especie")
 public class EspecieController {
+
+    @Autowired
+    private EspecieService especieService;
+
+    @GetMapping("/all")
+    public List<Especie> getAllAnimals() {
+        return especieService.getAllEspecie();
+    }
 }
--- src/main/java/api_adocao/Service/EspecieService.java
+++ src/main/java/api_adocao/Service/EspecieService.java
@@ -1,7 +1,20 @@
 package api_adocao.Service;
 
+import api_adocao.Model.Animal;
+import api_adocao.Model.Especie;
+import api_adocao.Repository.EspecieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class EspecieService {
+
+    @Autowired
+    private EspecieRepository especieRepository;
+
+    public List<Especie> getAllEspecie() {
+        return especieRepository.findAll();
+    }
 }
